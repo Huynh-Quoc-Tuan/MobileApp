2caec97ac72df0b7b45e1b265f7ede2ddd6160af
MinhGCC200321 <minhtqgcc200321@fpt.edu.vn>
2023-10-26 18:55:58 +0700

    Up code ne tuan

---
+++ app/src/main/java/com/example/a1786/AddHikeActivity.java
@@ -0,0 +1,4 @@
+package com.example.a1786;
+
+public class AddHikeActivity {
+}
---
+++ app/src/main/java/com/example/a1786/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.a1786;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.hike_res);
+    }
+}
